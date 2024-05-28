30ae9853e7ed114aa88673e6ec6e3b396adf816f
Jose Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2024-05-28 13:36:40 +0200

    Rebase from 'release/jazzy/mola_relocalization'

--- src/RelocalizationLikelihood_SE2.cpp
+++ src/RelocalizationLikelihood_SE2.cpp
@@ -26,6 +26,7 @@
 
 #include <mola_relocalization/relocalization.h>
 #include <mrpt/core/Clock.h>
+#include <mrpt/core/WorkerThreadsPool.h>
 #include <mrpt/version.h>
 
 #include <optional>
@@ -34,9 +35,11 @@
  * Algorithms for localization starting with large uncertainty.
  */
 
-mola::Relocalization_SE2::Output mola::Relocalization_SE2::run(const Input& in)
+// METHOD: likelihood
+mola::RelocalizationLikelihood_SE2::Output
+    mola::RelocalizationLikelihood_SE2::run(const Input& in)
 {
-    mola::Relocalization_SE2::Output result;
+    mola::RelocalizationLikelihood_SE2::Output result;
 
     const double t0 = mrpt::Clock::nowDouble();
 
@@ -59,13 +62,13 @@ mola::Relocalization_SE2::Output mola::Relocalization_SE2::run(const Input& in)
     // evaluate over the grid:
     std::optional<double> minW, maxW;
 
-    for (size_t iX = 0, iGlobal = 0; iX < nX; iX++, iGlobal++)
+    for (size_t iX = 0, iGlobal = 0; iX < nX; iX++)
     {
         const double x = grid.idx2x(iX);
         for (size_t iY = 0; iY < nY; iY++)
         {
             const double y = grid.idx2y(iY);
-            for (size_t iPhi = 0; iPhi < nPhi; iPhi++)
+            for (size_t iPhi = 0; iPhi < nPhi; iPhi++, iGlobal++)
             {
                 const double phi = grid.idx2phi(iPhi);
 
@@ -108,67 +111,4 @@ mola::Relocalization_SE2::Output mola::Relocalization_SE2::run(const Input& in)
     result.min_log_likelihood = *minW;
 
     return result;
-}
-
-std::map<double, mrpt::math::TPose2D> mola::find_best_poses_se2(
-    const mrpt::poses::CPosePDFGrid& grid, const double percentile)
-{
-    ASSERT_GT_(percentile, 0.0);
-    ASSERT_LT_(percentile, 1.0);
-
-#if MRPT_VERSION < 0x020c01
-    const size_t nX   = grid.getSizeX();
-    const size_t nY   = grid.getSizeY();
-    const size_t nPhi = grid.getSizePhi();
-
-    double maxLik = .0;
-    for (size_t iX = 0; iX < nX; iX++)
-        for (size_t iY = 0; iY < nY; iY++)
-            for (size_t iPhi = 0; iPhi < nPhi; iPhi++)
-            {
-                const double& cell = *grid.getByIndex(iX, iY, iPhi);
-                mrpt::keep_max(maxLik, cell);
-            }
-#else
-    const double maxLik =
-        *std::max_element(grid.data().begin(), grid.data().end());
-#endif
-
-    const double threshold = percentile * maxLik;
-
-    std::map<double, mrpt::math::TPose2D> best;
-
-#if MRPT_VERSION < 0x020c01
-    for (size_t iX = 0; iX < nX; iX++)
-    {
-        const double x = grid.idx2x(iX);
-        for (size_t iY = 0; iY < nY; iY++)
-        {
-            for (size_t iPhi = 0; iPhi < nPhi; iPhi++)
-            {
-                const double& cell = *grid.getByIndex(iX, iY, iPhi);
-                if (cell < threshold) continue;
-
-                const double y   = grid.idx2y(iY);
-                const double phi = grid.idx2phi(iPhi);
-
-                best[cell] = {x, y, phi};
-            }
-        }
-    }
-#else
-    const size_t N = grid.data().size();
-    for (size_t i = 0; i < N; i++)
-    {
-        const double cell = grid.data()[i];
-        if (cell < threshold) continue;
-
-        const auto [ix, iy, iphi] = grid.absidx2idx(i);
-        const double x            = grid.idx2x(ix);
-        const double y            = grid.idx2y(iy);
-        const double phi          = grid.idx2phi(iphi);
-        best[cell]                = {x, y, phi};
-    }
-#endif
-    return best;
 }
