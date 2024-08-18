4adcd321fa4fbd34d0fe5d348347558b0a4fc437
Jose Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2024-08-18 22:52:37 +0200

    Rebase from 'upstream'

--- mola_viz/src/mola_icon_64x64.h
+++ mola_viz/src/mola_icon_64x64.h
@@ -1,16 +1,18 @@
 /*  GIMP header image file format (RGB) */
 
-static const unsigned int mola_icon_width = 64;
+static const unsigned int mola_icon_width  = 64;
 static const unsigned int mola_icon_height = 64;
 
-/*  Call this macro repeatedly.  After each use, the pixel data can be extracted  */
+/*  Call this macro repeatedly.  After each use, the pixel data can be extracted
+ */
 
-#define HEADER_PIXEL(data,pixel) {\
-pixel[0] = (((data[0] - 33) << 2) | ((data[1] - 33) >> 4)); \
-pixel[1] = ((((data[1] - 33) & 0xF) << 4) | ((data[2] - 33) >> 2)); \
-pixel[2] = ((((data[2] - 33) & 0x3) << 6) | ((data[3] - 33))); \
-data += 4; \
-}
+#define HEADER_PIXEL(data, pixel)                                           \
+    {                                                                       \
+        pixel[0] = (((data[0] - 33) << 2) | ((data[1] - 33) >> 4));         \
+        pixel[1] = ((((data[1] - 33) & 0xF) << 4) | ((data[2] - 33) >> 2)); \
+        pixel[2] = ((((data[2] - 33) & 0x3) << 6) | ((data[3] - 33)));      \
+        data += 4;                                                          \
+    }
 // clang-format off
 static const char *mola_icon_data =
 	"````````````````````````````````````````````````````````````````"
