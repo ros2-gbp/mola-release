fbdeca6f67d3c5c1c04bd17c68dd608db097fffe
Jose Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2024-12-18 12:39:04 +0100

    Revert "Trimmed the branch to only the mola_navstate_fuse sub directory"
    This reverts commit e7e1deaa236d078d3abd6ee541f3fa2816e6dd29.

---
+++ mola_kernel/src/interfaces/MapServer.cpp
@@ -0,0 +1,19 @@
+/* -------------------------------------------------------------------------
+ *   A Modular Optimization framework for Localization and mApping  (MOLA)
+ * Copyright (C) 2018-2024 Jose Luis Blanco, University of Almeria
+ * See LICENSE for license information.
+ * ------------------------------------------------------------------------- */
+/**
+ * @file   MapServer.cpp
+ * @brief  Map saving/loading services offered by MOLA modules
+ * @author Jose Luis Blanco Claraco
+ * @date   Aug 18, 2024
+ */
+
+#include <mola_kernel/interfaces/MapServer.h>
+
+namespace mola
+{
+MapServer::MapServer()  = default;
+MapServer::~MapServer() = default;
+}  // namespace mola
