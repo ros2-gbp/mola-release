c9ae7b39fdda78bd2bc8efb6a081109f10a1e09b
Jose Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2024-12-18 12:47:44 +0100

    Rebase from 'upstream'

--- mola_kernel/include/mola_kernel/factors/SmartFactorStereoProjectionPose.h
+++ mola_kernel/include/mola_kernel/factors/SmartFactorStereoProjectionPose.h
@@ -23,7 +23,7 @@ class BackEndBase;
 
 /** Smart factor for a set of stereo-camera observations of one point landmark.
  * The ID of the factor is the ID of the landmark.
- * \ingroup mola_kernel_grp
+ * \ingroup mola_kernel_factors_grp
  */
 class SmartFactorStereoProjectionPose : public FactorBase
 {
