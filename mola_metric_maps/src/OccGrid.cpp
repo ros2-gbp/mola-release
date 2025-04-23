c2514fa986dbecaf8b8258c57ca11712b7b415a9
Jose Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2025-04-23 11:55:05 +0200

    Rebase from 'upstream'

--- mola_metric_maps/src/OccGrid.cpp
+++ mola_metric_maps/src/OccGrid.cpp
@@ -34,8 +34,8 @@ IMPLEMENTS_SERIALIZABLE(OccGrid, CSerializable, mola)
 
 OccGrid::OccGrid()
 {
-    // reset:
-    resetLikelihoodCache();
+  // reset:
+  resetLikelihoodCache();
 }
 
 // =====================================
@@ -45,24 +45,24 @@ OccGrid::OccGrid()
 uint8_t OccGrid::serializeGetVersion() const { return 0; }
 void    OccGrid::serializeTo(mrpt::serialization::CArchive& out) const
 {
-    // The data
-    out << grid_;
+  // The data
+  out << grid_;
 }
 void OccGrid::serializeFrom(mrpt::serialization::CArchive& in, uint8_t version)
 {
-    switch (version)
+  switch (version)
+  {
+    case 0:
     {
-        case 0:
-        {
-            in >> grid_;
-        }
-        break;
-        default:
-            MRPT_THROW_UNKNOWN_SERIALIZATION_VERSION(version);
-    };
+      in >> grid_;
+    }
+    break;
+    default:
+      MRPT_THROW_UNKNOWN_SERIALIZATION_VERSION(version);
+  };
 
-    // cache reset
-    resetLikelihoodCache();
+  // cache reset
+  resetLikelihoodCache();
 }
 
 // =====================================
@@ -70,54 +70,49 @@ void OccGrid::serializeFrom(mrpt::serialization::CArchive& in, uint8_t version)
 // =====================================
 
 void OccGrid::setSize(
-    const mrpt::math::TPoint2Df& minCorner,
-    const mrpt::math::TPoint2Df& maxCorner, float resolution,
-    float occupancyValue)
+    const mrpt::math::TPoint2Df& minCorner, const mrpt::math::TPoint2Df& maxCorner,
+    float resolution, float occupancyValue)
 {
-    grid_.setSize(
-        minCorner.x, maxCorner.x, minCorner.y, maxCorner.y, resolution,
-        occupancyValue);
+  grid_.setSize(minCorner.x, maxCorner.x, minCorner.y, maxCorner.y, resolution, occupancyValue);
 
-    resetLikelihoodCache();
+  resetLikelihoodCache();
 }
 
 void OccGrid::resizeGrid(
-    const mrpt::math::TPoint2Df& minCorner,
-    const mrpt::math::TPoint2Df& maxCorner, float newCellsOccupancy) noexcept
+    const mrpt::math::TPoint2Df& minCorner, const mrpt::math::TPoint2Df& maxCorner,
+    float newCellsOccupancy) noexcept
 {
-    grid_.resizeGrid(
-        minCorner.x, maxCorner.x, minCorner.y, maxCorner.y, newCellsOccupancy);
+  grid_.resizeGrid(minCorner.x, maxCorner.x, minCorner.y, maxCorner.y, newCellsOccupancy);
 
-    const auto defValue = CACHE_MISS_VALUE;
-    likelihoodCacheGrid_.resize(
-        grid_.getXMin(), grid_.getXMax(), grid_.getYMin(), grid_.getYMax(),
-        defValue, 0.5 /* extra margin [meters] */);
+  const auto defValue = CACHE_MISS_VALUE;
+  likelihoodCacheGrid_.resize(
+      grid_.getXMin(), grid_.getXMax(), grid_.getYMin(), grid_.getYMax(), defValue,
+      0.5 /* extra margin [meters] */);
 }
 
 // =====================================
 //   Read and update API
 // =====================================
 void OccGrid::insertObservation(
-    const mrpt::obs::CObservation2DRangeScan& obs,
-    const mrpt::math::TPose2D&                robotPose)
+    const mrpt::obs::CObservation2DRangeScan& obs, const mrpt::math::TPose2D& robotPose)
 {
-    // Update the grid:
-    auto&       gio = grid_.insertionOptions;
-    const auto& ip  = insertionParameters_;
+  // Update the grid:
+  auto&       gio = grid_.insertionOptions;
+  const auto& ip  = insertionParameters_;
 
-    gio.maxDistanceInsertion        = insertionParameters_.maxDistanceInsertion;
-    gio.maxOccupancyUpdateCertainty = ip.maxOccupancyUpdateCertainty;
-    gio.maxFreenessUpdateCertainty  = ip.maxFreenessUpdateCertainty;
-    gio.maxFreenessInvalidRanges    = ip.maxFreenessInvalidRanges;
-    gio.decimation                  = ip.decimation;
-    gio.wideningBeamsWithDistance   = ip.wideningBeamsWithDistance;
-    gio.considerInvalidRangesAsFreeSpace = ip.considerInvalidRangesAsFreeSpace;
+  gio.maxDistanceInsertion             = insertionParameters_.maxDistanceInsertion;
+  gio.maxOccupancyUpdateCertainty      = ip.maxOccupancyUpdateCertainty;
+  gio.maxFreenessUpdateCertainty       = ip.maxFreenessUpdateCertainty;
+  gio.maxFreenessInvalidRanges         = ip.maxFreenessInvalidRanges;
+  gio.decimation                       = ip.decimation;
+  gio.wideningBeamsWithDistance        = ip.wideningBeamsWithDistance;
+  gio.considerInvalidRangesAsFreeSpace = ip.considerInvalidRangesAsFreeSpace;
 
-    const auto p = mrpt::poses::CPose3D(robotPose);
-    grid_.insertObservation(obs, p);
+  const auto p = mrpt::poses::CPose3D(robotPose);
+  grid_.insertObservation(obs, p);
 
-    // Enqueue areas as pending of likelihood recalculation:
-    // TODO;
+  // Enqueue areas as pending of likelihood recalculation:
+  // TODO;
 }
 
 // =====================================
@@ -125,9 +120,8 @@ void OccGrid::insertObservation(
 // =====================================
 void OccGrid::resetLikelihoodCache()
 {
-    const auto defValue = CACHE_MISS_VALUE;
-    likelihoodCacheGrid_.setSize(
-        grid_.getXMin(), grid_.getXMax(), grid_.getYMin(), grid_.getYMax(),
-        grid_.getResolution() * likelihoodParameters_.superResolutionFactor,
-        &defValue);
+  const auto defValue = CACHE_MISS_VALUE;
+  likelihoodCacheGrid_.setSize(
+      grid_.getXMin(), grid_.getXMax(), grid_.getYMin(), grid_.getYMax(),
+      grid_.getResolution() * likelihoodParameters_.superResolutionFactor, &defValue);
 }
