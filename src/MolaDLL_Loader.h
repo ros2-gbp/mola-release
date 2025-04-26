ec53a75bf7e5f1369f68569d7107e01f1a9ef865
Scott K Logan <logans@cottsay.net>
2025-04-26 13:02:02 -0500

    Rebase from 'release/kilted/mola_launcher'

--- src/MolaDLL_Loader.h
+++ src/MolaDLL_Loader.h
@@ -20,14 +20,13 @@
 /** Used in internal_load_lib_modules() */
 struct LoadedModules
 {
-    std::string lib_path;
-    void*       handle{nullptr};
+  std::string lib_path;
+  void*       handle{nullptr};
 };
 
 /** Loads all libs under lib_search_paths_. \sa setup() */
 void internal_load_lib_modules(
-    mrpt::system::COutputLogger&    app,
-    const std::vector<std::string>& lib_search_paths);
+    mrpt::system::COutputLogger& app, const std::vector<std::string>& lib_search_paths);
 
 /** Returns the current list of loaded module dyanmic libraries. */
 const std::map<std::string, LoadedModules>& get_loaded_modules();
