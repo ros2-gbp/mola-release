bd6b45f74f1dc268fa75f9c857e743cd94bd40e5
Scott K Logan <logans@cottsay.net>
2025-04-26 13:02:20 -0500

    Rebase from 'rpm/kilted/mola_traj_tools'

--- src/traj_ypr2tum.cpp
+++ src/traj_ypr2tum.cpp
@@ -11,26 +11,26 @@
 
 int main(int argc, char** argv)
 {
-    try
+  try
+  {
+    if (argc != 3)
     {
-        if (argc != 3)
-        {
-            std::cerr << "Usage: " << argv[0] << " INPUT.ypr  OUTPUT.tum" << std::endl;
-            return 1;
-        }
-
-        const std::string sIn  = argv[1];
-        const std::string sOut = argv[2];
-
-        mrpt::poses::CPose3DInterpolator p;
-        p.loadFromTextFile(sIn);
-        p.saveToTextFile_TUM(sOut);
-
-        return 0;
+      std::cerr << "Usage: " << argv[0] << " INPUT.ypr  OUTPUT.tum" << std::endl;
+      return 1;
     }
-    catch (const std::exception& e)
-    {
-        std::cerr << "Exception: " << e.what() << std::endl;
-        return 1;
-    }
+
+    const std::string sIn  = argv[1];
+    const std::string sOut = argv[2];
+
+    mrpt::poses::CPose3DInterpolator p;
+    p.loadFromTextFile(sIn);
+    p.saveToTextFile_TUM(sOut);
+
+    return 0;
+  }
+  catch (const std::exception& e)
+  {
+    std::cerr << "Exception: " << e.what() << std::endl;
+    return 1;
+  }
 }
