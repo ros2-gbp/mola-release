beac8a5ee06a9f185c652d639386563510217977
Scott K Logan <logans@cottsay.net>
2025-04-26 13:01:52 -0500

    Rebase from 'release/kilted/mola_pose_list'

--- include/mola_pose_list/SearchablePoseList.h
+++ include/mola_pose_list/SearchablePoseList.h
@@ -38,52 +38,47 @@ namespace mola
  */
 class SearchablePoseList
 {
-   public:
-    SearchablePoseList() = default;
+ public:
+  SearchablePoseList() = default;
 
-    SearchablePoseList(bool measure_from_last_kf_only)
-        : from_last_only_(measure_from_last_kf_only)
-    {
+  SearchablePoseList(bool measure_from_last_kf_only) : from_last_only_(measure_from_last_kf_only) {}
+
+  bool empty() const
+  {
+    if (from_last_only_)  //
+      return last_kf_ == mrpt::poses::CPose3D::Identity();
+    else
+      return kf_poses_.empty();
+  }
+
+  size_t size() const { return from_last_only_ ? 1 : kf_poses_.size(); }
+
+  void insert(const mrpt::poses::CPose3D& p)
+  {
+    if (from_last_only_)
+    {  //
+      last_kf_ = p;
     }
-
-    bool empty() const
+    else
     {
-        if (from_last_only_)  //
-            return last_kf_ == mrpt::poses::CPose3D::Identity();
-        else
-            return kf_poses_.empty();
+      kf_points_.insertPoint(p.translation());
+      kf_poses_.push_back(p);
     }
+  }
 
-    size_t size() const { return from_last_only_ ? 1 : kf_poses_.size(); }
+  [[nodiscard]] std::tuple<bool /*isFirst*/, mrpt::poses::CPose3D /*distanceToClosest*/> check(
+      const mrpt::poses::CPose3D& p) const;
 
-    void insert(const mrpt::poses::CPose3D& p)
-    {
-        if (from_last_only_)
-        {  //
-            last_kf_ = p;
-        }
-        else
-        {
-            kf_points_.insertPoint(p.translation());
-            kf_poses_.push_back(p);
-        }
-    }
+  void removeAllFartherThan(const mrpt::poses::CPose3D& p, const double maxTranslation);
 
-    [[nodiscard]] std::tuple<
-        bool /*isFirst*/, mrpt::poses::CPose3D /*distanceToClosest*/>
-        check(const mrpt::poses::CPose3D& p) const;
+ private:
+  // if from_last_only_==true
+  mrpt::poses::CPose3D last_kf_ = mrpt::poses::CPose3D::Identity();
 
-    void removeAllFartherThan(
-        const mrpt::poses::CPose3D& p, const double maxTranslation);
+  // if from_last_only_==false
+  std::deque<mrpt::poses::CPose3D> kf_poses_;
+  mrpt::maps::CSimplePointsMap     kf_points_;
 
-   private:
-    // if from_last_only_==true
-    mrpt::poses::CPose3D last_kf_ = mrpt::poses::CPose3D::Identity();
-
-    // if from_last_only_==false
-    std::deque<mrpt::poses::CPose3D> kf_poses_;
-    mrpt::maps::CSimplePointsMap     kf_points_;
-
-    bool from_last_only_ = false;
+  bool from_last_only_ = false;
 };
 }  // namespace mola
