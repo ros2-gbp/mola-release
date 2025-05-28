3ed5ea33d64c920bcb310e74512ebe429225ff7e
Jose Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2025-05-28 13:48:27 +0200

    Revert "Trimmed the branch to only the mola_yaml sub directory"
    This reverts commit 332eb98202be94d7175a90b3727f3c27947feb5d.

---
+++ mola_kernel/include/mola_kernel/MinimalModuleContainer.h
@@ -0,0 +1,64 @@
+/* -------------------------------------------------------------------------
+ *   A Modular Optimization framework for Localization and mApping  (MOLA)
+ * Copyright (C) 2018-2025 Jose Luis Blanco, University of Almeria
+ * See LICENSE for license information.
+ * ------------------------------------------------------------------------- */
+/**
+ * @file   MinimalModuleContainer.h
+ * @brief  A simple module container for use without mola_launcher
+ * @author Jose Luis Blanco Claraco
+ * @date   Dec 26, 2024
+ */
+#pragma once
+
+#include <mola_kernel/interfaces/ExecutableBase.h>
+
+#include <string>
+#include <vector>
+
+namespace mola
+{
+/** \addtogroup mola_kernel_grp
+ * @{ */
+
+/**
+ * A minimal MOLA application container, for use programatically when it is
+ * not advisable to use the mola-cli application.
+ *
+ * This implements basic discoverability and visibility between modules.
+ */
+class MinimalModuleContainer
+{
+ public:
+  MinimalModuleContainer() = default;
+  ~MinimalModuleContainer();
+
+  MinimalModuleContainer(const std::vector<mola::ExecutableBase::Ptr>& mods) : modules_(mods)
+  {
+    for (auto& m : modules_)
+    {
+      ASSERT_(m);
+      installNameServer(*m);
+    }
+  }
+
+  void add(const mola::ExecutableBase::Ptr& m)
+  {
+    ASSERT_(m);
+    modules_.push_back(m);
+    installNameServer(*m);
+  }
+
+  const auto& modules() const { return modules_; }
+
+ private:
+  std::vector<mola::ExecutableBase::Ptr> modules_;
+
+  void installNameServer(mola::ExecutableBase& m);
+
+  ExecutableBase::Ptr nameServerImpl(const std::string& name);
+};
+
+/** @} */
+
+}  // namespace mola
