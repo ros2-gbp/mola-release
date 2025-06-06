fc6463c820ed2bad251203e08a6c2d95350d16d1
Jose Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2025-06-06 10:09:19 +0200

    Revert "Trimmed the branch to only the mola_input_kitti_dataset sub directory"
    This reverts commit 0774cf6fe9e688e8f0216f3dc15a52a01505ea8b.

---
+++ mola_kernel/include/mola_kernel/interfaces/MapServer.h
@@ -0,0 +1,58 @@
+/* -------------------------------------------------------------------------
+ *   A Modular Optimization framework for Localization and mApping  (MOLA)
+ * Copyright (C) 2018-2025 Jose Luis Blanco, University of Almeria
+ * See LICENSE for license information.
+ * ------------------------------------------------------------------------- */
+/**
+ * @file   MapServer.h
+ * @brief  Map saving/loading services offered by MOLA modules
+ * @author Jose Luis Blanco Claraco
+ * @date   Aug 18, 2024
+ */
+#pragma once
+
+#include <cstdlib>
+#include <string>
+
+namespace mola
+{
+/** Virtual interface for map server services offered by MOLA modules
+ * \ingroup mola_kernel_interfaces_grp */
+class MapServer
+{
+ public:
+  MapServer() = default;
+
+  /** @name Virtual interface of MapServer
+   *{ */
+
+  struct ReturnStatus
+  {
+    ReturnStatus() = default;
+
+    bool        success = false;
+    std::string error_message;
+  };
+
+  /** Loads a map from file(s) and sets it as active current map.
+   * Different implementations may use one or more files to store map as
+   * files.
+   *
+   *  \param[in] path File name(s) prefix for the map to load. Do not add file
+   * extension.
+   */
+  virtual ReturnStatus map_load([[maybe_unused]] const std::string& path) { return {}; }
+
+  /** Saves a map from file(s) and sets it as active current map.
+   * Different implementations may use one or more files to store map as
+   * files.
+   *
+   *  \param[in] path File name(s) prefix for the map to save. Do not add file
+   * extension.
+   */
+  virtual ReturnStatus map_save([[maybe_unused]] const std::string& path) { return {}; }
+
+  /** @} */
+};
+
+}  // namespace mola
