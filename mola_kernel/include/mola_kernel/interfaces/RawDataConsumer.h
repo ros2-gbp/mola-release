915f94dbf46ed9a1155d375a139d5959e47e97b5
Jose Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2025-07-07 00:09:06 +0200

    Revert "Trimmed the branch to only the mola_bridge_ros2 sub directory"
    This reverts commit 158e874882015e18aab240d051e783087d5e38bc.

---
+++ mola_kernel/include/mola_kernel/interfaces/RawDataConsumer.h
@@ -0,0 +1,37 @@
+/* -------------------------------------------------------------------------
+ *   A Modular Optimization framework for Localization and mApping  (MOLA)
+ * Copyright (C) 2018-2025 Jose Luis Blanco, University of Almeria
+ * See LICENSE for license information.
+ * ------------------------------------------------------------------------- */
+/**
+ * @file   RawDataConsumer.h
+ * @brief  Virtual interface for raw data consumers, typically, SLAM front-ends
+ * @author Jose Luis Blanco Claraco
+ * @date   Nov 21, 2018
+ */
+#pragma once
+
+#include <mrpt/obs/CObservation.h>
+
+namespace mola
+{
+using CObservation = mrpt::obs::CObservation;
+
+/** Virtual base for raw-observation consumers
+ * \ingroup mola_kernel_interfaces_grp */
+class RawDataConsumer
+{
+ public:
+  RawDataConsumer() = default;
+
+  /** @name Virtual interface of any RawDataConsumer
+   *{ */
+
+  /** To be called whenever a new observation arrives. It should return as
+   * fast as possible, enqueuing the data for processing in another thread.
+   */
+  virtual void onNewObservation(const CObservation::Ptr& o) = 0;
+  /** @} */
+};
+
+}  // namespace mola
