68d9930b0f5d99a71cde9952a382530bcd3e4295
Jose Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2025-10-13 18:15:07 +0200

    Rebase from 'upstream'

--- mola_input_kitti_dataset/include/mola_input_kitti_dataset/KittiOdometryDataset.h
+++ mola_input_kitti_dataset/include/mola_input_kitti_dataset/KittiOdometryDataset.h
@@ -1,8 +1,15 @@
-/* -------------------------------------------------------------------------
- *   A Modular Optimization framework for Localization and mApping  (MOLA)
- * Copyright (C) 2018-2025 Jose Luis Blanco, University of Almeria
- * See LICENSE for license information.
- * ------------------------------------------------------------------------- */
+/*               _
+ _ __ ___   ___ | | __ _
+| '_ ` _ \ / _ \| |/ _` | Modular Optimization framework for
+| | | | | | (_) | | (_| | Localization and mApping (MOLA)
+|_| |_| |_|\___/|_|\__,_| https://github.com/MOLAorg/mola
+
+ Copyright (C) 2018-2025 Jose Luis Blanco, University of Almeria,
+                         and individual contributors.
+ SPDX-License-Identifier: GPL-3.0
+ See LICENSE for full license information.
+*/
+
 /**
  * @file   KittiOdometryDataset.h
  * @brief  RawDataSource from Kitti odometry/SLAM datasets
@@ -93,6 +100,12 @@ class KittiOdometryDataset : public RawDataSourceBase,
  public:
   KittiOdometryDataset();
   ~KittiOdometryDataset() override = default;
+
+  // Prevent copying and moving
+  KittiOdometryDataset(const KittiOdometryDataset&)            = delete;
+  KittiOdometryDataset& operator=(const KittiOdometryDataset&) = delete;
+  KittiOdometryDataset(KittiOdometryDataset&&)                 = delete;
+  KittiOdometryDataset& operator=(KittiOdometryDataset&&)      = delete;
 
   // See docs in base class
   void         spinOnce() override;
