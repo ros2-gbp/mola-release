c6fa82484766197e2142e11ff1a1116905fb2646
Jose Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2025-10-13 18:20:49 +0200

    Rebase from 'upstream'

--- mola_metric_maps/include/mola_metric_maps/NDT.h
+++ mola_metric_maps/include/mola_metric_maps/NDT.h
@@ -1,22 +1,14 @@
-/* -------------------------------------------------------------------------
- *   A Modular Optimization framework for Localization and mApping  (MOLA)
- *
- * Copyright (C) 2018-2025 Jose Luis Blanco, University of Almeria
- * Licensed under the GNU GPL v3 for non-commercial applications.
- *
- * This file is part of MOLA.
- * MOLA is free software: you can redistribute it and/or modify it under the
- * terms of the GNU General Public License as published by the Free Software
- * Foundation, either version 3 of the License, or (at your option) any later
- * version.
- *
- * MOLA is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
- * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along with
- * MOLA. If not, see <https://www.gnu.org/licenses/>.
- * ------------------------------------------------------------------------- */
+/*               _
+ _ __ ___   ___ | | __ _
+| '_ ` _ \ / _ \| |/ _` | Modular Optimization framework for
+| | | | | | (_) | | (_| | Localization and mApping (MOLA)
+|_| |_| |_|\___/|_|\__,_| https://github.com/MOLAorg/mola
+
+ Copyright (C) 2018-2025 Jose Luis Blanco, University of Almeria,
+                         and individual contributors.
+ SPDX-License-Identifier: GPL-3.0
+ See LICENSE for full license information.
+*/
 
 /**
  * @file   NDT.h
@@ -43,7 +35,7 @@
 #include <functional>
 #include <optional>
 
-//#define HASHED_VOXEL_POINT_CLOUD_WITH_CACHED_ACCESS
+// #define HASHED_VOXEL_POINT_CLOUD_WITH_CACHED_ACCESS
 
 namespace mola
 {
@@ -65,6 +57,11 @@ class NDT : public mrpt::maps::CMetricMap,
 {
   DEFINE_SERIALIZABLE(NDT, mola)
  public:
+  NDT(const NDT&)            = default;
+  NDT& operator=(const NDT&) = default;
+  NDT(NDT&&)                 = default;
+  NDT& operator=(NDT&&)      = default;
+
   using global_index3d_t = index3d_t<int32_t>;
 
   /** @name Indices and coordinates
@@ -86,9 +83,9 @@ class NDT : public mrpt::maps::CMetricMap,
   inline mrpt::math::TPoint3Df globalIdxToCoord(const global_index3d_t idx) const
   {
     return {
-        idx.cx * voxel_size_,  //
-        idx.cy * voxel_size_,  //
-        idx.cz * voxel_size_};
+        static_cast<float>(idx.cx) * voxel_size_,  //
+        static_cast<float>(idx.cy) * voxel_size_,  //
+        static_cast<float>(idx.cz) * voxel_size_};
   }
 
   /// collapsed plain unique ID for global indices
@@ -161,7 +158,8 @@ class NDT : public mrpt::maps::CMetricMap,
 
     auto points() const -> PointSpan { return PointSpan(points_, nPoints_); }
 
-    void insertPoint(const mrpt::math::TPoint3Df& p, const mrpt::math::TPoint3Df& sensorPose)
+    void insertPoint(
+        const mrpt::math::TPoint3Df& p, const mrpt::math::TPoint3Df& sensorPose)  // NOLINT
     {
       if (nPoints_ < MAX_POINTS_PER_VOXEL)
       {
@@ -187,7 +185,7 @@ class NDT : public mrpt::maps::CMetricMap,
 
    private:
     mutable std::optional<mp2p_icp::PointCloudEigen> ndt_;
-    point_vector_t                                   points_;
+    point_vector_t                                   points_{};
     std::optional<mrpt::math::TPoint3Df>             was_seen_from_;
     uint32_t                                         nPoints_ = 0;
   };
@@ -237,9 +235,11 @@ class NDT : public mrpt::maps::CMetricMap,
       if (it == voxels_.end())
       {
         if (!createIfNew)
+        {
           return nullptr;
-        else
-          voxel = &voxels_[idx];  // Create it
+        }
+
+        voxel = &voxels_[idx];  // Create it
       }
       else
       {
@@ -295,7 +295,7 @@ class NDT : public mrpt::maps::CMetricMap,
       const std::function<void(const global_index3d_t&, const VoxelData&)>& f) const;
 
   /** Save to a text file. Each line contains "X Y Z" point coordinates.
-   *  Returns false if any error occured, true elsewere.
+   *  Returns false if any error ocurred, true elsewere.
    */
   bool saveToTextFile(const std::string& file) const;
 
