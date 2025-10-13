8428e7886bd03f77d6b4a19c1c34e8d071d45161
Jose Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2025-10-13 18:14:50 +0200

    Rebase from 'upstream'

--- docs/source/module-mola-input-kitti-dataset.rst
+++ docs/source/module-mola-input-kitti-dataset.rst
@@ -1,3 +1,3 @@
-:github_url: https://github.com/MOLAorg/mola-input-kitti-dataset/blob/master/docs/module-mola-input-kitti-dataset.rst
+:github_url: https://github.com/MOLAorg/mola-input-kitti-dataset/blob/develop/docs/module-mola-input-kitti-dataset.rst
 
 .. include:: ../../modules/mola-input-kitti-dataset/docs/module-mola-input-kitti-dataset.rst
--- mola_input_rawlog/include/mola_input_rawlog/RawlogDataset.h
+++ mola_input_rawlog/include/mola_input_rawlog/RawlogDataset.h
@@ -1,8 +1,15 @@
-/* -------------------------------------------------------------------------
- *   A Modular Optimization framework for Localization and mApping  (MOLA)
- * Copyright (C) 2018-2025 Jose Luis Blanco, University of Almeria
- * See LICENSE for license information.
- * ------------------------------------------------------------------------- */
+/*               _
+ _ __ ___   ___ | | __ _
+| '_ ` _ \ / _ \| |/ _` | Modular Optimization framework for
+| | | | | | (_) | | (_| | Localization and mApping (MOLA)
+|_| |_| |_|\___/|_|\__,_| https://github.com/MOLAorg/mola
+
+ Copyright (C) 2018-2025 Jose Luis Blanco, University of Almeria,
+                         and individual contributors.
+ SPDX-License-Identifier: GPL-3.0
+ See LICENSE for full license information.
+*/
+
 /**
  * @file   RawlogDataset.h
  * @brief  RawDataSource for datasets in MRPT rawlog format
@@ -41,10 +48,13 @@ class RawlogDataset : public RawDataSourceBase, public OfflineDatasetSource, pub
   size_t datasetUI_size() const override
   {
     if (read_all_first_)
+    {
       return datasetSize();
-    else
-      return 10000000;  // we just don't know...
+    }
+
+    return 10000000;  // we just don't know...
   }
+
   size_t datasetUI_lastQueriedTimestep() const override
   {
     auto lck = mrpt::lockHelper(dataset_ui_mtx_);
