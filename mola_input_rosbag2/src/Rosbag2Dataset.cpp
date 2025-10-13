50965019a86b380656996c3aed9d38636557c651
Jose Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2025-10-13 18:38:24 +0200

    Rebase from 'upstream'

--- mola_input_rosbag2/src/Rosbag2Dataset.cpp
+++ mola_input_rosbag2/src/Rosbag2Dataset.cpp
@@ -1,8 +1,15 @@
-/* -------------------------------------------------------------------------
- *   A Modular Optimization framework for Localization and mApping  (MOLA)
- * Copyright (C) 2018-2025 Jose Luis Blanco, University of Almeria
- * See LICENSE for license information.
- * ------------------------------------------------------------------------- */
+/*               _
+ _ __ ___   ___ | | __ _
+| '_ ` _ \ / _ \| |/ _` | Modular Optimization framework for
+| | | | | | (_) | | (_| | Localization and mApping (MOLA)
+|_| |_| |_|\___/|_|\__,_| https://github.com/MOLAorg/mola
+
+ Copyright (C) 2018-2025 Jose Luis Blanco, University of Almeria,
+                         and individual contributors.
+ SPDX-License-Identifier: GPL-3.0
+ See LICENSE for full license information.
+*/
+
 /**
  * @file   Rosbag2Dataset.h
  * @brief  RawDataSource for datasets in rosbag2 format
@@ -308,8 +315,7 @@ void Rosbag2Dataset::initialize_rds(const Yaml& c)
     }
     else if (sensorType == "CObservationRotatingScan")
     {
-      auto callback = [=](const rosbag2_storage::SerializedBagMessage& m)
-      {
+      auto callback = [=](const rosbag2_storage::SerializedBagMessage& m) {
         return catchExceptions([=]() { return toRotatingScan(sensorLabel, m, fixedSensorPose); });
       };
       lookup_[topic].emplace_back(callback);
