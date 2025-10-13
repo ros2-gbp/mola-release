b8462b21d679d64c2f3876f53136939f0c15a26b
Jose Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2025-10-13 18:30:28 +0200

    Rebase from 'upstream'

--- kitti_metrics_eval/apps/kitti-metrics-eval.cpp
+++ kitti_metrics_eval/apps/kitti-metrics-eval.cpp
@@ -1,8 +1,14 @@
-/* -------------------------------------------------------------------------
- *   A Modular Optimization framework for Localization and mApping  (MOLA)
- * Copyright (C) 2018-2025 Jose Luis Blanco, University of Almeria
- * See LICENSE for license information.
- * ------------------------------------------------------------------------- */
+/*               _
+ _ __ ___   ___ | | __ _
+| '_ ` _ \ / _ \| |/ _` | Modular Optimization framework for
+| | | | | | (_) | | (_| | Localization and mApping (MOLA)
+|_| |_| |_|\___/|_|\__,_| https://github.com/MOLAorg/mola
+
+ Copyright (C) 2018-2025 Jose Luis Blanco, University of Almeria,
+                         and individual contributors.
+ SPDX-License-Identifier: GPL-3.0
+ See LICENSE for full license information.
+*/
 
 /**
  * @file   kitti-metrics-eval
