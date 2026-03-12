da7346382f9493396bc88560bff56e2264c4bf01
Jose Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2026-03-12 09:33:07 +0100

    Rebase from 'debian/jazzy/mola_pose_list'

--- src/SearchablePoseList.cpp
+++ src/SearchablePoseList.cpp
@@ -1,22 +1,14 @@
-/* -------------------------------------------------------------------------
- *   A Modular Optimization framework for Localization and mApping  (MOLA)
- *
- * Copyright (C) 2018-2025 Jose Luis Blanco, University of Almeria
- * Licensed under the GNU GPL v3 for non-commercial applications.
- *
- * This file is part of MOLA.
- * MOLA is free software: you can redistribute it and/or modify it under the
- * terms of the GNU General Public License as published by the Free Software
- * Foundation, either version 3 of the License, or (at your option) any later
- * version.
- *
- * MOLA is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
- * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along with
- * MOLA. If not, see <https://www.gnu.org/licenses/>.
- * ------------------------------------------------------------------------- */
+/*               _
+ _ __ ___   ___ | | __ _
+| '_ ` _ \ / _ \| |/ _` | Modular Optimization framework for
+| | | | | | (_) | | (_| | Localization and mApping (MOLA)
+|_| |_| |_|\___/|_|\__,_| https://github.com/MOLAorg/mola
+
+ Copyright (C) 2018-2026 Jose Luis Blanco, University of Almeria,
+                         and individual contributors.
+ SPDX-License-Identifier: GPL-3.0
+ See LICENSE for full license information.
+*/
 /**
  * @file   SearchablePoseList.cpp
  * @brief  Data structure to search for nearby SE(3) poses
@@ -34,10 +26,13 @@ std::tuple<bool /*isFirst*/, mrpt::poses::CPose3D /*distanceToClosest*/> Searcha
 {
   const bool           isFirst = empty();
   mrpt::poses::CPose3D distanceToClosest;
-  if (isFirst) return {isFirst, distanceToClosest};
+  if (isFirst)
+  {
+    return {isFirst, distanceToClosest};
+  }
 
   if (from_last_only_)
-  {  //
+  {
     distanceToClosest = p - last_kf_;
   }
   else
@@ -63,9 +58,12 @@ std::tuple<bool /*isFirst*/, mrpt::poses::CPose3D /*distanceToClosest*/> Searcha
       const auto&  candidate = kf_poses_.at(closestID.at(i));
       const double rot = mrpt::poses::Lie::SO<3>::log((p - candidate).getRotationMatrix()).norm();
 
-      closestSqrDist[i] += ROTATION_WEIGHT * mrpt::square(rot);
+      closestSqrDist[i] += static_cast<float>(ROTATION_WEIGHT * mrpt::square(rot));
 
-      if (!bestIdx || closestSqrDist[i] < closestSqrDist[*bestIdx]) bestIdx = i;
+      if (!bestIdx || closestSqrDist[i] < closestSqrDist[*bestIdx])
+      {
+        bestIdx = i;
+      }
     }
 
     const auto& closestPose = kf_poses_.at(closestID.at(*bestIdx));
@@ -79,7 +77,10 @@ std::tuple<bool /*isFirst*/, mrpt::poses::CPose3D /*distanceToClosest*/> Searcha
 void SearchablePoseList::removeAllFartherThan(
     const mrpt::poses::CPose3D& p, const double maxTranslation)
 {
-  if (from_last_only_) return;  // not applicable
+  if (from_last_only_)
+  {
+    return;  // not applicable
+  }
 
   std::deque<mrpt::poses::CPose3D> new_kf_poses;
   mrpt::maps::CSimplePointsMap     new_kf_points;
@@ -91,13 +92,16 @@ void SearchablePoseList::removeAllFartherThan(
   {
     mrpt::math::TPoint3D pt;
     kf_points_.getPoint(i, pt.x, pt.y, pt.z);
-    if ((pt - c).sqrNorm() > maxSqrDist) continue;  // remove
+    if ((pt - c).sqrNorm() > maxSqrDist)
+    {
+      continue;  // remove
+    }
     // pass:
     new_kf_points.insertPoint(pt);
     new_kf_poses.push_back(kf_poses_.at(i));
   }
   // replace:
   kf_poses_  = std::move(new_kf_poses);
-  kf_points_ = std::move(new_kf_points);
+  kf_points_ = std::move(new_kf_points);  // NOLINT
   ASSERT_EQUAL_(kf_poses_.size(), kf_points_.size());
 }
