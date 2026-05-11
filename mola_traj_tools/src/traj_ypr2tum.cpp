2df658d9af5bc301460e7a3f51287cf5259658cd
Jose Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2026-05-11 23:11:09 +0200

    Revert "Trimmed the branch to only the mola_bridge_ros2 sub directory"
    This reverts commit 01a45827fe3cd8e8c2234c49be0ddf3280353c79.

---
+++ mola_traj_tools/src/traj_ypr2tum.cpp
@@ -0,0 +1,43 @@
+/*               _
+ _ __ ___   ___ | | __ _
+| '_ ` _ \ / _ \| |/ _` | Modular Optimization framework for
+| | | | | | (_) | | (_| | Localization and mApping (MOLA)
+|_| |_| |_|\___/|_|\__,_| https://github.com/MOLAorg/mola
+
+ Copyright (C) 2018-2026 Jose Luis Blanco, University of Almeria,
+                         and individual contributors.
+ SPDX-License-Identifier: GPL-3.0
+ See LICENSE for full license information.
+*/
+
+#include <mrpt/core/exceptions.h>
+#include <mrpt/poses/CPose3DInterpolator.h>
+
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+  try
+  {
+    if (argc != 3)
+    {
+      std::cerr << "Usage: " << argv[0] << " INPUT.ypr  OUTPUT.tum"
+                << "\n";
+      return 1;
+    }
+
+    const std::string sIn  = argv[1];
+    const std::string sOut = argv[2];
+
+    mrpt::poses::CPose3DInterpolator p;
+    p.loadFromTextFile(sIn);
+    p.saveToTextFile_TUM(sOut);
+
+    return 0;
+  }
+  catch (const std::exception& e)
+  {
+    std::cerr << "Exception: " << e.what() << "\n";
+    return 1;
+  }
+}
